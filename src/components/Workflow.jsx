f16d23f2fbd25f5b0d73574d85a782a4deb844c2
Hashit <raizadaharshit2004@gmail.com>
2024-09-02 20:35:12 +0530

    ok

--- src/components/Workflow.jsx
+++ src/components/Workflow.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Workflow = () => {
-  return <div></div>;
+  return (
+    <div className="relative mt-10 ">
+      <div className="text-center">
+        <span></span>
+        <div className="">
+          <h2 className="flex text-3xl sm:text-6xl lg:text-7xl lg:mt-20 sm:mt-10 tracking-wide ">
+            Accelerate your
+            <span className="bg-gradient-to-r from-orange-500 to-orange-800 bg-neutral-900 text-transparent bg-clip-text">
+              {" "}
+              coding workflow
+            </span>
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Workflow;
