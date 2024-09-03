0d5f12cf70b5232b4f816e38328099bdbb8309fd
Hashit <raizadaharshit2004@gmail.com>
2024-09-03 15:02:12 +0530

    okkkk

--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { pricingOptions } from "../constants/index";
+import { CheckCircle2 } from "lucide-react";
 
 const Price = () => {
   return (
@@ -19,6 +20,24 @@ const Price = () => {
                   </span>
                 )}
               </p>
+              <p className="mb-8">
+                <span className="text-5xl mt-6 mr-2">{item.price}</span>
+                <span className="text-neutral-400 tracking-tight"></span>
+              </p>
+              <ul>
+                {item.features.map((feature, index) => (
+                  <li key={index} className="mt-8 flex items-center">
+                    <CheckCircle2 />
+                    <span className="ml-2">{feature}</span>
+                  </li>
+                ))}
+              </ul>
+              <a
+                href="#"
+                className="inline-flex justify-center items-center text-center w-full h-12 p-5 mt-20 tracking-tight text-xl hover:bg-orange-900 border border-orange-900 rounded-lg transition duration:200"
+              >
+                Subscribe
+              </a>
             </div>
           </div>
         ))}
